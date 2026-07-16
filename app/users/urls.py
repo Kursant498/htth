084d8240c1da009fe27313aa0b43bf1381a18eb2
Kursant-cell <k4739709@gmail.com>
2026-07-17 00:43:10 +0600

    home2 commit

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from app.users.views import RegisterViewSet, LoginViewSet
+from app.users.views import RegisterViewSet, LoginViewSet, LogoutViewSet, UserAuthenticatedViewSet
 
 router = DefaultRouter()
 
 router.register("register", RegisterViewSet, basename="register")
 router.register("login", LoginViewSet, basename="login")
-
+router.register("logout", LogoutViewSet, basename="logout")
+router.register("userauth", UserAuthenticatedViewSet, basename="userauth")
 urlpatterns = router.urls
